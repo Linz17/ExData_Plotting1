51e5a0edc6d0634150509ae3f2c95aac7ea93fd5
Marie <Linz17@users.noreply.github.com>
2015-11-08 17:13:56 +0000

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+## fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+## download.file(fileurl, destfile = "hpc.zip", method = "curl")
+## unzip("hpc.zip")
+hpc <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+dates <- grep("2007-02-01|2007-02-02", hpc$Date)
+feb <- hpc[dates,]
+feb$Global_active_power <- as.numeric(as.character(feb$Global_active_power))
+hist(feb$Global_active_power, col = "orangered", main = "Global Active Power", 
+    xlab = "Global Active Power (kilowatts)")
