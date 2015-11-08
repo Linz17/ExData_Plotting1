eeb46a997c152ff6a13dfaacdd43a217c8968f21
Marie <Linz17@users.noreply.github.com>
2015-11-08 18:10:56 +0000

    Adding PNG creation code

--- plot4.R
+++ plot4.R
@@ -11,19 +11,21 @@ feb$Sub_metering_1 <- as.numeric(as.character(feb$Sub_metering_1))
 feb$Sub_metering_2 <- as.numeric(as.character(feb$Sub_metering_2))
 feb$Global_reactive_power <- as.numeric(as.character(feb$Global_reactive_power))
 feb$Voltage <- as.numeric(as.character(feb$Voltage))
+
+png(file = "plot4.png", width=480,height=480,units="px")
+
 par(mfrow = c(2, 2))
-with({
-  plot(feb$Time, feb$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+
+plot(feb$Time, feb$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
   
-  plot(feb$Time, feb$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(feb$Time, feb$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
   
-  plot(feb$Time, feb$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
-  lines(feb$Time, feb$Sub_metering_1, type = "l", col = "black")
-  lines(feb$Time, feb$Sub_metering_2, type = "l", col = "red")
-  lines(feb$Time, feb$Sub_metering_3, type = "l", col = "blue")
-  legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), 
+plot(feb$Time, feb$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+lines(feb$Time, feb$Sub_metering_1, type = "l", col = "black")
+lines(feb$Time, feb$Sub_metering_2, type = "l", col = "red")
+lines(feb$Time, feb$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), 
          lwd=c(2.5,2.5), col=c("black", "red", "blue"), bty = "n")
-  
-  plot(feb$Time, feb$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
-  
-})
+plot(feb$Time, feb$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
+dev.off()
