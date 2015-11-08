6b07969f63b0babe834198d70e198cffd4e27531
Marie <Linz17@users.noreply.github.com>
2015-11-08 17:46:57 +0000

    Adding PNG creattion code

--- plot3.R
+++ plot3.R
@@ -9,9 +9,14 @@ x <- paste(as.character(feb$Date), feb$Time)
 feb$Time <- strptime(x, "%Y-%m-%d %H:%M:%S")
 feb$Sub_metering_1 <- as.numeric(as.character(feb$Sub_metering_1))
 feb$Sub_metering_2 <- as.numeric(as.character(feb$Sub_metering_2))
+
+png(file = "plot3.png", width=480,height=480,units="px")
+
 plot(feb$Time, feb$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
 lines(feb$Time, feb$Sub_metering_1, type = "l", col = "black")
 lines(feb$Time, feb$Sub_metering_2, type = "l", col = "red")
 lines(feb$Time, feb$Sub_metering_3, type = "l", col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), 
     lwd=c(2.5,2.5), col=c("black", "red", "blue"))
+
+dev.off()
